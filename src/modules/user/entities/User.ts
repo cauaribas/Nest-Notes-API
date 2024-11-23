ea47fc82aab23834c165dd4074e17b66ba20ceb8
Cauã Ribas <100866872+cauaribas@users.noreply.github.com>
2024-11-23 00:59:53 -0300

    feat: create User entity with properties and methods for user management

---
+++ src/modules/user/entities/User.ts
@@ -0,0 +1,63 @@
+import { randomUUID } from 'crypto';
+import { Replace } from 'src/utils/replace';
+
+interface UserSchema {
+  name: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export class User {
+  props: UserSchema;
+  _id: string;
+
+  constructor(
+    props: Replace<UserSchema, { createdAt?: Date; updatedAt?: Date }>,
+    id?: string,
+  ) {
+    this.props = {
+      ...props,
+      createdAt: props.createdAt || new Date(),
+      updatedAt: props.updatedAt || new Date(),
+    };
+    this._id = id || randomUUID();
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  get email(): string {
+    return this.props.email;
+  }
+
+  set email(email: string) {
+    this.email = email;
+  }
+
+  get password(): string {
+    return this.props.password;
+  }
+
+  set password(password: string) {
+    this.password = password;
+  }
+
+  get name(): string {
+    return this.props.name;
+  }
+
+  set name(name: string) {
+    this.name = name;
+  }
+
+  get createdAt(): Date {
+    return this.props.createdAt;
+  }
+
+  get updatedAt(): Date {
+    return this.props.updatedAt;
+  }
+}
