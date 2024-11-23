b753b4128d03ccfb5a9171ef66b2195fe039337e
Cauã Ribas <100866872+cauaribas@users.noreply.github.com>
2024-11-23 14:35:44 -0300

    feat: implement local authentication strategy using Passport.js

---
+++ src/modules/auth/strategies/local.strategy.ts
@@ -0,0 +1,21 @@
+import { Strategy } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ValidateUserUseCase } from '../useCases/validateUserUseCase/validateUserUseCase';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private validateUserUseCase: ValidateUserUseCase) {
+    super({
+      usernameField: 'email',
+      passwordField: 'password',
+    });
+  }
+
+  async validate(email: string, password: string) {
+    return await this.validateUserUseCase.execute({
+      email,
+      password,
+    });
+  }
+}
