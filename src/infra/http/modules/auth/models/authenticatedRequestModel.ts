68c8447cfcd5fc3f7e1bfe883afadaff7e11e1c6
Cauã Ribas <100866872+cauaribas@users.noreply.github.com>
2024-11-24 00:03:32 -0300

    feat: create AuthenticatedRequestModel to extend Express Request with user details

---
+++ src/infra/http/modules/auth/models/authenticatedRequestModel.ts
@@ -0,0 +1,11 @@
+import { Request } from 'express';
+
+export class AuthenticatedRequestModel extends Request {
+  user: {
+    id: string;
+    email: string;
+    name: string;
+    createdAt: string;
+    updatedAt: string;
+  };
+}
